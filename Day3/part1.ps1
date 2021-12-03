7846be2c3b64f88e2021dce7986ce08f99cda24e
Arko de Leeuw <arko.de.leeuw@sogeti.com>
2021-12-03 15:00:38 +0100

    Array vervangen door string

--- Day3/part1.ps1
+++ Day3/part1.ps1
@@ -1,24 +1,24 @@
 $code = Get-Content Day3/input.txt
 $index = 0
-$gamma = @(0,0,0,0,0,0,0,0,0,0,0,0)
-$epsilon = @(0,0,0,0,0,0,0,0,0,0,0,0)
+$gamma = ""
+$epsilon = ""
 
 while ($index -lt 12){
     $measure = $code.Substring($index,1) | Measure-Object -Average -Sum
 
     if ($measure.Average -ge 0.5){
-        $gamma[$index] = 1
-        $epsilon[$index] = 0
+        $gamma += "1"
+        $epsilon += "0"
     } else {
-        $gamma[$index] = 0
-        $epsilon[$index] = 1
+        $gamma += "0"
+        $epsilon += "1"
     }
     $index++
 }
 
 
-$gammaD = [Convert]::ToInt32(-join $gamma,2)
-$epsilonD = [Convert]::ToInt32(-join $epsilon,2)
+$gammaD = [Convert]::ToInt32($gamma,2)
+$epsilonD = [Convert]::ToInt32($epsilon,2)
 
 Write-Host "Gamma: " $gammaD
 Write-Host "Epsilon: " $epsilonD
